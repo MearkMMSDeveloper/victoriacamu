96e1729c0a0b7f5f2ddada6f89e22f3339f73840
MearkMMSDeveloper <meark.marketer@gmail.com>
2026-03-20 18:27:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/camu/",
+   base: "/victoriacamu/",
   server: {
     host: "::",
     port: 8080,
