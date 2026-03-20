b81a007034a463383bd2801bde756bfaf8266081
MearkMMSDeveloper <meark.marketer@gmail.com>
2026-03-20 18:28:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Sonner />
-      <BrowserRouter basename="/camu">
+      <BrowserRouter basename="/victoriacamu">
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
